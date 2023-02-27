06cc037e9de68bb7f2315066921f2764b9466a0e
p1rv <karol.p.krol@gmail.com>
2023-02-27 21:14:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  document.title = "Futurum | CRUD";
+
+  return (
+    <div className="flex items-center justify-center">
+      <div>App</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface ICampaignEntry {
+  bid_amount: number;
+  campaign_fund: number;
+  campaign_name: string;
+  keywords: string[];
+  radius: number;
+  status: boolean;
+  town: string;
+}
