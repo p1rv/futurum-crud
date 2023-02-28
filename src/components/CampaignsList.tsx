4115b30f5bb2838555f945abcd1e1534fa73774a
p1rv <karol.p.krol@gmail.com>
2023-02-28 16:09:45 +0100

    added campaign add boilerplate form

--- src/components/CampaignsList.tsx
+++ src/components/CampaignsList.tsx
@@ -1,11 +1,22 @@
 import { CampaignItem } from "./CampaignItem";
 import { useFetchCampaignsQuery } from "../store";
+import { CampaignForm } from "./CampaignForm";
+import { CampaignAdd } from "./CampaignAdd";
+import { useState } from "react";
 
 export const CampaignsList: React.FC = () => {
   const { data, error, isFetching } = useFetchCampaignsQuery(null);
+  const [showAddForm, setShowAddForm] = useState(false);
 
   return (
-    <div>
+    <div className="w-full flex flex-col justify-center">
+      <button
+        className="bg-blue-500 px-8 py-4 rounded-2xl my-6 text-xl"
+        onClick={() => setShowAddForm(true)}
+      >
+        + Add Campaign
+      </button>
+      {showAddForm && <CampaignAdd closeForm={() => setShowAddForm(false)} />}
       {data &&
         data.map((campaign) => (
           <CampaignItem
