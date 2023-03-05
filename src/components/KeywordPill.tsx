7facd6de1bf8c2a6a54e891ba04ccaf4f8c6d884
p1rv <karol.p.krol@gmail.com>
2023-03-05 18:11:05 +0100

    fixed x's uneven position on different clients

--- src/components/KeywordPill.tsx
+++ src/components/KeywordPill.tsx
@@ -32,12 +32,26 @@ export const KeywordPill: React.FC<ITypeaheadTokenArgs> = ({ keyword, props }) =
     "group-hover:bg-white/50",
     "ml-1",
     "relative",
-    "after:content-['×']",
+    "after:content-['']",
+    "after:w-3",
+    "after:border-black",
+    "after:rotate-[45deg]",
+    "after:border-t-2",
     "after:absolute",
     "after:top-1/2",
     "after:left-1/2",
     "after:-translate-x-1/2",
-    "after:-translate-y-1/2"
+    "after:-translate-y-1/2",
+    "before:content-['']",
+    "before:w-3",
+    "before:border-black",
+    "before:-rotate-[45deg]",
+    "before:border-t-2",
+    "before:absolute",
+    "before:top-1/2",
+    "before:left-1/2",
+    "before:-translate-x-1/2",
+    "before:-translate-y-1/2"
   );
 
   return (
